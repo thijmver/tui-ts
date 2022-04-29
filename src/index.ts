9fdffc4fafd85639578f556b3b7a15a0964700a8
thijm <59415467+thijmver@users.noreply.github.com>
2022-04-29 23:29:31 +0200

    Generate type definitions on compile

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 import { Builder } from "./structures/builder";
+
 export const { createContextMenu, select, build, getState, setState } = Builder;
+
+export * from "./structures/builder";
+export * from "./structures/contextMenu";
